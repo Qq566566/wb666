4d83b4ae51bca357be8e630471164d1b7ae071d0
Qq566566 <tin566095@gmail.com>
2026-07-29 02:54:15 +0800

    Set default value for description in blog schema
    Update blog collection schema to set default description.

--- src/content.config.ts
+++ src/content.config.ts
@@ -2,11 +2,10 @@ import { defineCollection, z } from 'astro:content';
 import { glob } from 'astro/loaders';
 
 const blog = defineCollection({
-  // 使用 Astro 5+ 推荐的 glob loader 加载 Markdown 文件
   loader: glob({ pattern: '**/[^_]*.md', base: "./src/content/blog" }),
   schema: z.object({
     title: z.string(),
-    description: z.string(),
+    description: z.string().default(''),
     pubDate: z.union([z.string(), z.date()]).transform((val) => {
       const parsed = new Date(val);
       return isNaN(parsed.getTime()) ? new Date() : parsed;
