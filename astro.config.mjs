fbcb5fa3b1cbc5d04a7e02e9277fd928e6916741
Qq566566 <tin566095@gmail.com>
2026-07-28 23:48:45 +0800

    Add sitemap integration to Astro config

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,6 +1,8 @@
 import { defineConfig } from 'astro/config';
+import sitemap from '@astrojs/sitemap';
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://blog.wb666.im',
+  site: 'https://blog.wb666.im', 
+  integrations: [sitemap()],
 });
